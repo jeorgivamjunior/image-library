b1ba1733dc60aeb4fa6010adf13df6926b868569
Jeorgivam Junior <JeorgivamJunior@gmail.com>
2022-10-15 00:59:30 +0100

    Add ImagePage tests

--- src/pages/ImagePage/tests/ImagePage.test.tsx
+++ src/pages/ImagePage/tests/ImagePage.test.tsx
@@ -1,6 +1,24 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+
+import { ImageContextProvider, ModalContextProvider } from '../../../contexts';
+import { ImagePage } from '../ImagePage';
 
 describe('ImagePage file', () => {
-  // should render the title
-  // should render the the add new image button
+  it('should render the title, button, and table', async () => {
+    render(
+      <ModalContextProvider>
+        <ImageContextProvider>
+          <ImagePage />
+        </ImageContextProvider>
+      </ModalContextProvider>,
+    );
+
+    const tittle = screen.getByRole('heading', { name: 'Images' });
+    const button = screen.getByRole('button', { name: 'Add new image' });
+    const table = screen.getByRole('grid');
+
+    expect(table).toBeInTheDocument();
+    expect(tittle).toBeInTheDocument();
+    expect(button).toBeInTheDocument();
+  });
 });
