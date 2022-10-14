015406911ac27819be1446f07aeff80f8287c2ca
Jeorgivam Junior <JeorgivamJunior@gmail.com>
2022-10-15 00:42:47 +0100

    Add GridList tests

--- src/pages/ImagePage/tests/GridList.test.tsx
+++ src/pages/ImagePage/tests/GridList.test.tsx
@@ -1,6 +1,50 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+import { GridList } from '../components';
 
 describe('GridList file', () => {
-  // should render a table with 2 items
+  it('should render a table with 2 items', () => {
+    const props = {
+      imageList: [{ id: 1 }, { id: 2 }],
+    } as any;
+
+    render(<GridList {...props} />);
+
+    const tableRows = screen.getAllByRole('row');
+
+    // including the header row
+    expect(tableRows).toHaveLength(3);
+  });
+
+  it('should render a table with 2 items', () => {
+    const props = {
+      imageList: [{ id: 1 }, { id: 2 }],
+    } as any;
+
+    render(<GridList {...props} />);
+
+    const tableRows = screen.getAllByRole('row');
+
+    // including the header row
+    expect(tableRows).toHaveLength(3);
+  });
+
+  it('should display the table with 3 items', () => {
+    const props = {
+      imageList: [{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }, { id: 5 }, { id: 6 }],
+    } as any;
+
+    render(<GridList {...props} />);
+
+    const nextPageButton = screen.getByLabelText('Go to next page');
+
+    userEvent.click(nextPageButton);
+
+    const tableRows = screen.getAllByRole('row');
+
+    // including the header row
+    expect(tableRows).toHaveLength(2);
+  });
   // should render the sixth item on the screen after click on the next page
 });
